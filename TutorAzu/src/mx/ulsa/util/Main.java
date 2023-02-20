d97d134fb9408011bdf2ed15b1c00fded7771585
Azucena27 <61021132+Azucena27@users.noreply.github.com>
2023-02-20 11:20:13 -0600

    Main

--- TutorAzu/src/mx/ulsa/util/Main.java
+++ TutorAzu/src/mx/ulsa/util/Main.java
@@ -2,14 +2,17 @@ package mx.ulsa.util;
 
 import org.w3c.dom.Text;
 public class Main {
+	private static String rutaOrigen = "C:\\archivos\\";
+	private static String rutaDestino = "\\\\175.1.44.45\\archivos\\destino";
+	
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
 		
 		for(int i = 0; i < 7; i++){
 			
-			TextThread hilo1 = new TextThread("C:\\archivos\\datos\\misdatos.txt", "misdatos"+i+".txt", "\\\\175.1.43.125\\archivos\\destino");
-			TextThread hilo3 = new TextThread("C:\\archivos\\datos\\msword.docx", "msword"+i+".docx", "\\\\175.1.43.125\\archivos\\destino");
-			TextThread hilo2 = new TextThread("C:\\archivos\\multimedia\\Video 5MB.mp4", "video 5MB"+i+".mp4", "\\\\175.1.43.125\\archivos\\destino");
+			TextThread hilo1 = new TextThread(rutaOrigen+"datos\\misdatos.txt", "misdatos"+i+".txt", rutaDestino);
+			TextThread hilo3 = new TextThread(rutaOrigen+"datos\\msword.docx", "msword"+i+".docx", rutaDestino);
+			TextThread hilo2 = new TextThread(rutaOrigen+"multimedia\\Video 5MB.mp4", "video 5MB"+i+".mp4", rutaDestino);
 			hilo1.run();
 			hilo2.run();
 			hilo3.run();
